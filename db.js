a51576708395342434f66e6d1b86479768383d04
hadas <m0527170266@gmail.com>
2026-07-14 22:27:35 +0300

    add books data in db.js

---
+++ db.js
@@ -0,0 +1,29 @@
+// db.js
+
+const books = [
+  {
+    code: "101",
+    title: "The Great Gatsby",
+    category: "Classic Literature",
+    price: 49.90,
+    isBorrowed: false,
+    borrowHistory: [] 
+  },
+  {
+    code: "102",
+    title: "The Hobbit",
+    category: "Fantasy",
+    price: 59.90,
+    isBorrowed: true,
+    borrowHistory: [
+      {
+        borrowDate: "2026-07-01",
+        customerId: "cust_505"
+      }
+    ]
+  }
+];
+
+module.exports = {
+  books
+};
