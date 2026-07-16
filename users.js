293f8a08f44d68a4cba96a032ce8162828b8796c
hadas <m0527170266@gmail.com>
2026-07-16 21:02:06 +0300

    add users.js

---
+++ users.js
@@ -0,0 +1,18 @@
+const users = [
+  {
+    code: "1",
+    username: "user_israel",
+    email: "israel@example.com",
+    password: "password123",
+    borrowedBooks: [] 
+  },
+  {
+    code: "2",
+    username: "user_sara",
+    email: "sara@example.com",
+    password: "password456",
+    borrowedBooks: []
+  }
+];
+
+export default users;
