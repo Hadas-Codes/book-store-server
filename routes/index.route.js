09d4ea5a05526f9e78bbfacd8364b085f426f504
hadas <m0527170266@gmail.com>
2026-07-16 22:38:56 +0300

    add routeIndex and change app

---
+++ routes/index.route.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import bookRouter from './book.route.js';
+import userRouter from './user.route.js';
+
+const router = express.Router();
+
+router.use('/books', bookRouter);
+router.use('/users', userRouter);
+
+export default router;
